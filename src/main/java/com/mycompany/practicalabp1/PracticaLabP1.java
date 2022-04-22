c3b56d5fc3f87199ce793d83560d3a44f0307544
excal <excal@LAPTOP-ACVGNVK1>
2022-04-22 15:15:27 -0600

    El anexo de la clase alumno

---
+++ src/main/java/com/mycompany/practicalabp1/PracticaLabP1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.practicalabp1;
+
+/**
+ *
+ * @author excal
+ */
+public class PracticaLabP1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!" );
+        System.out.println("- Valor Numérico: "+(10+12.34)+"$ \nEstado: "+true+" - Habilitado");
+        System.out.print("Datos de alumnos\nCuenta: 22211176\nNombre Alumno: Wilson Guerra\nCarrera: Ingeniería en Sistemas\n");
+    }
+}
